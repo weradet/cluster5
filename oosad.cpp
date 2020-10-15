9386761363cae32ea68dd708a6da67beaabcc8cc
weradet <70476442+weradet@users.noreply.github.com>
2020-10-15 15:17:14 +0700

    Update oosad.cpp

--- oosad.cpp
+++ oosad.cpp
@@ -1,55 +1,13 @@
 #include"fluk.h"
 #include"station.h"
+#include"Ui.h"
 #include<conio.h>
 using namespace std;
  /*created by weradet nopsombun 62160110 2/10/63
    Class UI 
    function main 
  */  
-class UI{
-    private:
-    RenewControler *Renew_obj;  
-    public:
-       UI(){
-           Renew_obj = new RenewControler;
-       }
-       void PrintCustomer(){
-           //print customer menu
-          cout << "*****Electic Train*****" << endl;
-          cout << "1. Find cycle Time" << endl; 
-          cout << "2. Buy Train Ticket" << endl;
-          cout << "3. ";
-       }//printcustomer menu
-       void PrintAdmin(){
-           //print employee menu
-           //print interface
-           cout << "*****Electic Train*****" << endl;
-           cout << "1. Find cycle Time" << endl; 
-           cout << "2. Buy Train Ticket" << endl;
-           cout << "3. Register Member" << endl;
-           cout << "4. Renew Member Card" << endl;
-           cout << "5. Add Station" << endl;
-           cout << "6. Fix Station" << endl;
-           cout << "7. Back" << endl;
-           cout << "Please Choose 1 Choice(1-7): ";
 
-       } // print Admin
-       void PrintFirstMenu(){
-        //print the first menu
-        cout << "1.Customer" << endl;
-        cout << "2.Admin" << endl;
-        cout << "3.Out" << endl;
-        cout << "Please Choose 1 Choice(1-3): ";
-        }//print FirstMenu
-      void CheckMenuAdmin(int menu){
-            //check is menu admin in the method PrintAdmin() in UI class
-            if(menu == 4){
-            //Renew Card
-               cout << "Hello";   
-            }//if
-        }
-
-};
 int main(){
      //mainmenu
      int Menu;
