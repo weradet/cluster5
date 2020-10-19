2f9693678708ceab4f98116e882e53df4e362de9
weradet <70476442+weradet@users.noreply.github.com>
2020-10-19 15:01:24 +0700

    Create domainclass.h

---
+++ domainclass.h
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class MemberCard{
+       private:
+         string IdCard;
+         double Money;
+         string DayExpire;  
+       public:
+         MemberCard(){
+         }
+         void SetMemberCard(string id_card){
+             IdCard = id_card; 
+         }    
+};
+
+class Member{
+    //class member 
+      private:
+        string Name,Lastname,Tel;
+        MemberCard member_card; 
+        /*data is name of member lastname 
+        Telephone number 
+        Thai Id Card */ 
+      public: 
+      Member *next;
+      void Setmember(string user_ame,string lastname,string tel,string id_card){
+        //set up member
+          Name = user_ame;
+          Lastname = lastname;
+          Tel = tel;
+           member_card.SetMemberCard(id_card);  
+      }//set up member
+};
+
+class ListMember{
+//Linked list Member 
+      private:
+        Member *head;
+        Member *tail;
+      public:
+         ListMember(){
+             head = NULL;
+             tail = NULL;
+         }
+         void Addmember(Member newmember){
+           //Add data to the linklist
+           Member *new_member = new Member;
+            if(head == NULL){
+              head = new_member;
+              tail = new_member; 
+             }else{
+               tail->next = new_member;
+               tail = new_member;
+               } 
+             }
+};
