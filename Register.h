7c85c80120fbd00930eb524aae81addc36f609da
NSD-NK <62160085@go.buu.ac.th>
2020-10-19 16:00:36 +0700

    Create Register.h

---
+++ Register.h
@@ -0,0 +1,16 @@
+#include "include.h"
+class RegisterController{
+    private:
+        /* data */
+    public:
+    Register(/* args */);
+    ~Register();
+};
+
+Register::Register(/* args */){
+
+}
+
+Register::~Register(){
+
+}
