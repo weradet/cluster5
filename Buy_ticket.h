37a0663ce4917d8cc38bcb514f807e735080713e
wachiravit44 <garfieldandfield@gmail.com>
2020-10-19 15:35:00 +0700

    Update Buy_ticket.h

--- Buy_ticket.h
+++ Buy_ticket.h
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
-class Buy_ticket{
+class Buy_ticket_controller{
     private:
-
+        string terminal;
+        string departure;
+        int cost;
+        string round;
     public:
-
+        void Seach();
+        void Departure_Staion();
+        void Terminal_Staion();
+        void Chose_Round();
+        void Calculate();
+        void Payment();
+        void Print_Ticket();
 };
 
