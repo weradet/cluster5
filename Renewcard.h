15a4f3d4a6adb9c6bbcbc6d35ac661dfca7cfdac
weradet <70476442+weradet@users.noreply.github.com>
2020-10-19 15:01:52 +0700

    Update Renewcard.h

--- Renewcard.h
+++ Renewcard.h
@@ -1,70 +1,14 @@
 /* Create By Weradet Nopsombun 62160110 
    Use Case - Renew Card
-            - Register Member
     *** Add class MemberList
 */ 
 #include<iostream>
 #include<string>
 #include<fstream>
 #include<iomanip>
+#include"domainclass.h"
 using namespace std;
-
 //Class
-class MemberCard{
-       private:
-         string IdCard;
-         double Money;
-         string DayExpire;  
-       public:
-         MemberCard(){
-         }
-         void SetMemberCard(string id_card){
-             IdCard = id_card; 
-         }    
-};
-
-class Member{
-    //class member 
-      private:
-        string Name,Lastname,Tel;
-        MemberCard member_card; 
-        /*data is name of member lastname 
-        Telephone number 
-        Thai Id Card */ 
-      public: 
-      Member *next;
-      void Setmember(string user_ame,string lastname,string tel,string id_card){
-        //set up member
-          Name = user_ame;
-          Lastname = lastname;
-          Tel = tel;
-           member_card.SetMemberCard(id_card);  
-      }//set up member
-};
-
-class ListMember{
-//Linked list Member 
-      private:
-        Member *head;
-        Member *tail;
-      public:
-         ListMember(){
-             head = NULL;
-             tail = NULL;
-         }
-         void Addmember(Member newmember){
-           //Add data to the linklist
-           Member *new_member = new Member;
-            if(head == NULL){
-              head = new_member;
-              tail = new_member; 
-             }else{
-               tail->next = new_member;
-               tail = new_member;
-               } 
-             }
-};
-
 class RenewControler{
     //Contro Usecase Renew Card
 private:
@@ -75,7 +19,7 @@ public:
         cout << "****Renew Card****" << endl;
         cout << "1.Renew Card" << endl;
         cout << "2. Back " << endl;
-    }//print renewcard
+    }
 };
 
 // Function
