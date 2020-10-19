6feffb56d354039c5d33389892f43774c466d9dc
62160150 <62160150@go.buu.ac.th>
2020-10-19 16:28:38 +0700

    Update station.h

--- station.h
+++ station.h
@@ -24,6 +24,13 @@ class Station{
         int Cost;
         Station *link;
     public:
+        Station(string id,string sta_name,string sta_stu,int cost){
+            ID = id;
+            StationName = sta_name;
+            StationStatus = sta_stu;
+            Cost = cost;
+            link = NULL;
+        }
         void printShowAllStation(){
             cout << "------------------ Station -----------------" << endl;
             cout << "ID Station" << setw(10) << right << "Station Name" << setw(10) << right 
@@ -42,11 +49,5 @@ class Station{
         }
         int cost(){
             return Cost;
-        }
-        void Maintenance(){
-
-        }
-        void FixCostStation(){
-            
         }
 };
