aa0436046df3a89400f7d00da85e2d674714498b
wachiravit44 <garfieldandfield@gmail.com>
2020-10-19 16:04:51 +0700

    Update Path.h

--- Path.h
+++ Path.h
@@ -1,6 +1,28 @@
+#include "include.h"
 class Path{
     private:
-        
-    public:
 
+    public:
+        linklist_station *head;
+        linklist_station *tail;
+        void add_path(string name){
+            linklist_station *New = new linklist_station(name);
+            if(head == NULL){
+                
+            }else{
+
+            }
+        }
+};
+
+class linklist_station{
+    public:
+        linklist_station *link;
+        linklist_station *plink;
+        string station_name;
+        linklist_station(string name){
+            station_name = name;
+            link = NULL;
+            plink = NULL;
+        }
 }
