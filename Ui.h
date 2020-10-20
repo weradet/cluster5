56743524cdd879655a292da9d00fb9d566e0419a
62160102 <62160102@go.buu.ac.th>
2020-10-20 21:52:04 +0700

    Update Ui.h

--- Ui.h
+++ Ui.h
@@ -1,9 +1,9 @@
 #include"include.h"
-#include"station.h"
+//#include"station.h"
 #include"domainclass.h"
 #include"Register.h"
 #include"Renewcard.h"
-
+#include"Buy_ticket.h"
 class UI{
     /*
     class UI 
@@ -12,11 +12,13 @@ class UI{
     private:
    RenewController *Renew_obj;  
    RegisterController *Regis_obj;
+   Buy_ticket_controller *a;
     //StationController *station_obj;
     public:
        UI(){
             Renew_obj = new RenewController;
             Regis_obj = new RegisterController;
+            a = new Buy_ticket_controller;
           //station_obj = new StationController;
        }
        void PrintCustomer(){
@@ -62,10 +64,17 @@ class UI{
             Regis_obj->SetMemberInformation(firstname,lastname,tel,password);   
         }//inputpersonal
         void ShowExprirationDate(string name){
+<<<<<<< Updated upstream
             Regis_obj->ShowExpireCard(name);
+=======
+              Regis_obj->showExpireCard(name);
+>>>>>>> Stashed changes
         }
         void ShowInformation(string name){
-            Regis_obj->ShowUserInformation(name);
+            Regis_obj->ShowuserInformation(name);
+        }
+        void printfile(){
+             a->Departure_Staion("Departure");
         }
         void InputMoney(double money){
             Regis_obj->InputMoney(money);
