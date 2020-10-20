39c489b85232dea2f79f144789fd08fde94ad2d4
NSD-NK <62160085@go.buu.ac.th>
2020-10-20 20:44:19 +0700

    Update Ui.h

--- Ui.h
+++ Ui.h
@@ -15,31 +15,33 @@ class UI{
     //StationController *station_obj;
     public:
        UI(){
-           Renew_obj = new RenewController;
+            Renew_obj = new RenewController;
             Regis_obj = new RegisterController;
-          // station_obj = new StationController;
+          //station_obj = new StationController;
        }
        void PrintCustomer(){
-           //print customer menu
-          cout << "*****Electic Train*****" << endl;
-          cout << "1. Find cycle Time" << endl; 
-          cout << "2. Buy Train Ticket" << endl;
-          cout << "3. Have a member" << endl;
-          cout << "4. Back to the main menu" << endl;
-          cout << "Please Choose 1 Choice(1-4): ";
+            //print customer menu
+            cout << "***************Electic Train***************" << endl;
+            cout << "1. Find cycle Time" << endl; 
+            cout << "2. Buy Train Ticket" << endl;
+            cout << "3. Have a member" << endl;
+            cout << "4. Back to the main menu" << endl;
+            cout << "*******************************************" << endl;
+            cout << "Please Choose 1 Choice (1-4) : ";
        }//printcustomer menu
        void PrintAdmin(){
            //print employee menu
            //print interface
-           cout << "*****Electic Train*****" << endl;
-           cout << "1. Find cycle Time" << endl; 
-           cout << "2. Buy Train Ticket" << endl;
-           cout << "3. Register Member" << endl;
-           cout << "4. Renew Member Card" << endl;
-           cout << "5. Add Station" << endl;
-           cout << "6. Fix Station" << endl;
-           cout << "7. Back" << endl;
-           cout << "Please Choose 1 Choice(1-7): ";
+            cout << "***************Electic Train***************" << endl;
+            cout << "1. Find cycle Time" << endl; 
+            cout << "2. Buy Train Ticket" << endl;
+            cout << "3. Register Member" << endl;
+            cout << "4. Renew Member Card" << endl;
+            cout << "5. Add Station" << endl;
+            cout << "6. Fix Station" << endl;
+            cout << "7. Back" << endl;
+            cout << "*******************************************" << endl;
+            cout << "Please Choose 1 Choice (1-7) : ";
        } // print Admin
 
        void PrintFirstMenu(){
@@ -47,7 +49,7 @@ class UI{
         cout << "1.Customer" << endl;
         cout << "2.Admin" << endl;
         cout << "3.Out" << endl;
-        cout << "Please Choose 1 Choice(1-3): ";
+        cout << "Please Choose 1 Choice (1-3) : ";
         }//print FirstMenu
       /*void CheckMenuAdmin(int menu){
             //check is menu admin in the method PrintAdmin() in UI class
@@ -56,13 +58,13 @@ class UI{
                cout << "Hello";   
             }//if
         }*/
-        void InputPersonalInformation(string name,string lastname,string tel,string passwordcard){
-             Regis_obj->SetMemberInformation(name,lastname,tel,passwordcard);   
+        void InputPersonalInformation(string firstname,string lastname,string tel,string password){
+             Regis_obj->SetMemberInformation(firstname,lastname,tel,password);   
         }//inputpersonal
         void ShowExprirationDate(string name){
-              Regis_obj->showExpireCard(name);
+              Regis_obj->ShowExpireCard(name);
         }
         void ShowInformation(string name){
-            Regis_obj->ShowuserInformation(name);
+            Regis_obj->ShowUserInformation(name);
         }
 };
