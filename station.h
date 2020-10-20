53869919d387734b9850656b7a7f29f9bdb2aab2
62160150 <62160150@go.buu.ac.th>
2020-10-20 14:56:26 +0700

    Update station.h

--- station.h
+++ station.h
@@ -9,21 +9,13 @@ class StationController{
             cout << "3. Fix Cost Station"       << endl;
             cout << "--------------------------------------------" << endl;
         }
-        void printShowAllStation(){
-            cout << "------------------ Station -----------------" << endl;
-            cout << "ID Station" << setw(10) << right << "Station Name" << setw(10) << right 
-                 << "Status" << setw(10) << right << "Cost" << endl;
-            
-            cout << "--------------------------------------------" << endl;
-        }
-    
 };
 class Station{
     private:
         string ID,StationName,StationStatus;
         int Cost;
-        Station *link;
     public:
+        Station *link;
         Station(string id,string sta_name,string sta_stu,int cost){
             ID = id;
             StationName = sta_name;
@@ -31,12 +23,14 @@ class Station{
             Cost = cost;
             link = NULL;
         }
-        void printShowAllStation(){
-            cout << "------------------ Station -----------------" << endl;
-            cout << "ID Station" << setw(10) << right << "Station Name" << setw(10) << right 
-                 << "Status" << setw(10) << right << "Cost" << endl;
-            
-            cout << "--------------------------------------------" << endl;
+        void NewCost(int cost){
+            Cost = cost;
+        }
+        void NewStatus(string status){
+            StationStatus = status;
+        }
+        void NewName(string name){
+            StationName = name;
         }
         string StationID(){
             return ID;
@@ -50,4 +44,68 @@ class Station{
         int cost(){
             return Cost;
         }
+};
+class Status :protected Add{
+    public:
+        void printShowAllStation(){
+            Station *temp = head;
+            int i;
+            cout << "------------------ Station -----------------" << endl;
+            cout << "ID Station" << setw(10) << right << "Station Name" << setw(10) << right 
+                 << "Status" << setw(10) << right << "Cost" << endl;
+            while(temp != NULL){
+                cout << left << setw(10) << i;
+                cout << left << setw(10) << temp->StationID();
+                cout << left << setw(10) << temp->stationName();
+                cout << left << setw(10) << temp->stationStatus();
+                cout << left << setw(10) << temp->cost() << endl;
+            }
+            cout << "--------------------------------------------" << endl;
+        }
+        void printStation(string id){
+            Station *temp = head;
+            cout << "------------------ Detail Station -----------------" << endl;
+            while(temp != NULL){
+                if(id == temp->ID){
+                   cout << "Station ID : " << temp->ID << endl;
+                   cout << "Station Name : " << temp->StationName << endl;
+                   cout << "Station Status : " << temp->StationStatus << endl;
+                   cout << "Station Cost : " << temp->Cost << endl; 
+                }
+               temp = temp->link;
+            }
+            cout << "--------------------------------------------" << endl;
+        }
+        void FixCostStation(string id,int cost){
+            Station *temp = head;
+            int ptemp;
+            while(temp != NULL){
+                if(id == temp->ID){
+                    temp->NewCost(cost);
+                    break;
+                }
+                temp = temp->link;
+            }
+        }
+        void Maintenence(srting id){
+            Station *temp = head;
+            while(temp != NULL){
+                if(id == temp->ID){
+                    temp->NewStatus("Close");
+                    break;
+                }
+                temp = temp->link;
+            }
+        }
+        void NewStationName(string id,string name){
+           Station *temp = head;
+            while(temp != NULL){
+                if(id == temp->ID){
+                    temp->Newname(name);
+                    break;
+                }
+                temp = temp->link;
+            } 
+        }
+
 };
