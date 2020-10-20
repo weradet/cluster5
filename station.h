bbb6d08d49df147088aaff23e6fbcd411f864ee1
62160150 <62160150@go.buu.ac.th>
2020-10-20 18:49:33 +0700

    Update station.h

--- station.h
+++ station.h
@@ -26,6 +26,7 @@ class Station{
             StationStatus = sta_stu;
             Cost = cost;
             link = NULL;
+            plink = NULL;
         }
         void NewCost(int cost){
             Cost = cost;
