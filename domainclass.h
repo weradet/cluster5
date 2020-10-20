ad24d60fd8f581798a32504dc56223f91191a65d
NSD-NK <62160085@go.buu.ac.th>
2020-10-20 16:28:14 +0700

    Update domainclass.h

--- domainclass.h
+++ domainclass.h
@@ -1,34 +1,34 @@
 #include"include.h"
 class MemberCard{
-       private:
-         string IdCard;
-         double Money;
-         string DayExpire;  
-       public:
-         MemberCard(){
-         }
-         void SetMemberCard(string id_card){
-             IdCard = id_card; 
-         }    
+      private:
+        string IdCard;
+        double Money;
+        string DayExpire;  
+      public:
+        MemberCard(){
+        }
+        void SetMemberCard(string id_card){
+            IdCard = id_card; 
+        }    
 };
 
 class Member{
     //class member 
       private:
-        string Name,Lastname,Tel;
+        string Firstname,Lastname,Tel;
         MemberCard member_card; 
         /*data is name of member lastname 
         Telephone number 
         Thai Id Card */ 
       public: 
-      Member *link;
-      void Setmember(string user_ame,string lastname,string tel,string id_card){
-        //set up member
-          Name = user_ame;
-          Lastname = lastname;
-          Tel = tel;
-           member_card.SetMemberCard(id_card);  
-      }//set up member
+        Member *link;
+        void Setmember(string user_ame,string lastname,string tel,string id_card){
+          //set up member
+            Firstname = user_ame;
+            Lastname = lastname;
+            Tel = tel;
+            member_card.SetMemberCard(id_card);  
+        }//set up member
 };
 
 class ListMember{
@@ -37,20 +37,20 @@ class ListMember{
         Member *head;
         Member *tail;
       public:
-         ListMember(){
-             head = NULL;
-             tail = NULL;
-         }
-         void Addmember(Member newmember){
+        ListMember(){
+            head = NULL;
+            tail = NULL;
+        }
+        void Addmember(Member newmember){
            //Add data to the linklist
-           Member *new_member = new Member;
+            Member *new_member = new Member;
            
             if(head == NULL){
               head = new_member;
               tail = new_member; 
-             }else{
-               tail->link = new_member;
-               tail = new_member;
-               } 
-             }
+            }else{
+              tail->link = new_member;
+              tail = new_member;
+            } 
+        }
 };
