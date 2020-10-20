6d76606116db70214c6c7aeee4a19ded5fa4db5b
62160150 <62160150@go.buu.ac.th>
2020-10-20 21:57:10 +0700

    Update Round.h

--- Round.h
+++ Round.h
@@ -1,17 +1,80 @@
 #include "include.h"
 class Round{
-        private :
-            string TimeOut;
-            string TimeTo;
-        
-        public :
-            void SearchPath(string DepartureName, string TerminalName){
-
-
+        private:
+            string TimeOut[9];
+            string Name;
+        public:
+            Round *head;
+            Round *tail;
+            Round *link;
+            Round *plink;
+            int count;
+            Round(){
+            	head = NULL;
+            	tail = NULL;
+            	link = NULL;
+            	plink = NULL;
+            	count = 0;
+			}
+            Round(string timeout,int i){
+                TimeOut[i] = timeout;
             }
-
-            void PrintTicket(string DepartureName, string TerminalName, string Round, int Cost){
-
-                
+            Round(string name){
+                Name = name;
             }
+            void add(string name){
+                Round *temp = new Round(name);
+                if(head == NULL){
+                    head = temp;
+                    tail = temp;
+                    count++;
+                }else{
+                    tail->link = temp;	
+				    temp->plink = tail;
+				    tail = temp;
+                    count++;
+                }
+            }
+            void add(string timeout,int i){
+                Round *temp = new Round(timeout,i);
+                if(head == NULL){
+                    head = temp;
+                    tail = temp;
+                    count++;
+                }else{
+                    tail->link = temp;	
+				    temp->plink = tail;
+				    tail = temp;
+                    count++;
+                }
+            }
+            void Readfile(){
+                string filein,name,timeout[9];
+                ifstream file("test2.txt",ios::in);
+                if(file.is_open()){
+                    while(getline(file,filein)){
+                        name = filein.substr(0,filein.find(','));
+						     filein.erase(0,filein.find(',')+1);
+                        for(int i=0;i<9;i++){
+                            timeout[i] = filein.substr(0,filein.find(','));
+						     filein.erase(0,filein.find(',')+1);
+                            add(timeout[i],i);
+                        }
+                    add(name);
+                    }
+                //file.close();
+                }
+            }
+            void Show(){
+            	Round *temp = head;
+            
+                while(temp != NULL){
+            		cout << temp->Name;
+            		for(int i=0;i<9;i++){
+            			cout << temp->TimeOut[i];
+					}
+					cout << endl;
+					temp = temp->link;
+				}
+			}
 };
