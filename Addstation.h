505a2c81cc0c1b7cd93c5b6ecc6791a51cec2882
ธนิสร  ธรรมสวนิต <62160088@go.buu.ac.th>
2020-10-21 00:30:31 +0700

    Update Addstation.h

--- Addstation.h
+++ Addstation.h
@@ -1,62 +1,72 @@
 #include<iostream>
-#include<string>
 #include<fstream>
-#include<iomanip>
 using namespace std;
-class station_Data{
+class Node_Addstation{
 	public:
 		string StationName,StationID,Status,Cost;
-        int index;
-		Station_Data *link;
-		Station_Data *plink;
-		Station_Data(int index,string Sid,string name,string stat,string c){
+		Node_Addstation *link;
+		Node_Addstation *plink;
+		Node_Addstation(string Sid,string name,string stat,string c){
+			 StationID = Sid;
 			StationName = name;
-            StationID = Sid;
             Status = stat;
             Cost = c;
 			link = NULL;
 			plink = NULL;
 		}
 };
-class stationList{
-    private:
-        Station_Data *head;
-        Station_Data *tail;
-        int count;
-    public:
-        string StationName,StationID,Status;
-        int Cost;
-        int countList(){
-            int num = 0;
-            Station_Data *temp = head;
-            while(temp!=NULL){
-                temp = temp->link;
-                num++;
-            }
-            return num;
-        }
-        void AddOntheWay(string StationID,string StationName,string Status,string Cost){
-			Station_Data *n = new Station_Data(StationID,StationName,Status,Cost);
-			Station_Data *temp = head;
-            int index;
-            //stringstream ss;  
-            //ss << StationID;  
-            //ss >> index;  
-			if(index > count){
-				cout << "cannot add" << endl;
-			}else if(index == 1){				
-				n->link = head;
-				head->plink = n;
+class StationList{
+	private:		
+		Node_Addstation *head;
+		Node_Addstation *tail;
+		int count;
+	public:
+		StationList(){
+			head = NULL;
+			tail = NULL;
+			count = 0;
+		}
+		~StationList(){
+			for(int i=1;i<count;i++){
+				Node_Addstation *tmp = head;
+				head = head->link;
+				delete tmp;
+				tmp = NULL;
+			}
+			head = NULL;
+			tail = NULL;
+		}
+		void add(string StationID,string StationName,string Status,string Cost){
+			Node_Addstation *n = new Node_Addstation(StationID,StationName,Status,Cost);
+			if(head == NULL){
 				head = n;
+				tail = head;
+				count++;
+			}else{
+				tail->link = n;
+				n->plink = tail;	
+				tail = n;
+				count++;
+			}		
+		}
+		void add(int index,string StationID,string StationName,string Status,string Cost){
+			Node_Addstation *newnode = new Node_Addstation(StationID,StationName,Status,Cost);
+			Node_Addstation *temp = head;
+			if(index > count){
+				cout << "Overflow" << endl;
+			}else if(index == 1){				
+				newnode->link = head;
+				head->plink = newnode;
+				head = newnode;
 				count++;
 			}else{
 				int i=2;
 				while(temp!=NULL){
 					if(index == i){
-						n->plink = temp;
-						n->link = temp->link;
-						temp->link->plink = n;
-						temp->link = n;
+						newnode->plink = temp;
+						newnode->link = temp->link;
+						temp->link->plink = newnode;
+						temp->link = newnode;
 					}
 					temp = temp->link;
 					i++;
@@ -64,92 +74,139 @@ class stationList{
 				count++;
 			}				
 		}
-        void AddStation(){
-            string Cost;
+		bool checkStationID(string id){
+			Node_Addstation *temp = head;
+			while(temp !=NULL){
+				if(temp->StationID == id){
+					return true;
+					break;
+				}
+				temp = temp->link;
+			}
+		}
+		bool checkStationName(string name){
+			Node_Addstation *temp = head;
+			while(temp !=NULL){
+				if(temp->StationName == name){
+					return true;
+					break;
+				}
+				temp = temp->link;
+			}
+		}
+		void show(){
+			Node_Addstation *temp = head;
+			int i = 1;
+			while(temp != NULL){
+				cout << "List " << i << ": = " << temp->StationID << temp->StationName << temp->Status << temp->Cost << endl;
+				temp = temp->link;
+				i++;
+			}		
+		}
+		void writefile(){
+			string line3;
+			Node_Addstation *temp = head;
+			ofstream myFile3("Station.txt",ios::app);
+        	if(myFile3.is_open()){ 
+        	while(temp!=NULL){
+				myFile3 << temp->StationID <<","<< temp->StationName << "," << temp->Status  << "," << temp->Cost << endl;
+				temp = temp->link;
+   			}
+		}
+
+		}
+		int size(){
+            return count;
+        }
+};
+int main(){
+	StationList *stList = new StationList;
+	char choice;
+	string StationName,StationID,Status,Cost;
+    do{
+Addstation:cout <<"1.Add destination"<<endl;
+		cout <<"2.Add Departure station,Add station on the way"<<endl;
+		cout <<"3.show "<<endl;
+		cout <<"4.Exit "<<endl;
+		cout <<"Enter ==> ";
+        cin >> choice;
+        if(choice == '1'){
+            //string Cost;
                 cout << "Station ID: ";
                 cin >> StationID;
                     while(StationID.length() != 5){
-                        cout << "!!! Warring !!!\n";
+                        cout << "!!! Warning !!!\n";
                         cout << "Station ID Not equal to 5 ";
                         cin >> StationID;
                     }
                 cout << "StationName: ";
                 cin >> StationName;
+				if(stList->checkStationID(StationID)==true && stList->checkStationName(StationName)==true){
+					cout << "The station already exists!!!" << endl;
+				}
                 cout << "Station Status: ";
                 cin >> Status;
                 cout << "Station cost: ";
                 cin >> Cost;
-                ofstream Stationfile;
-                Stationfile.open("Station.txt",std::ios::app);
-                Stationfile << StationID <<","<< StationName << "," << Status  << "," << Cost << endl;
-                Stationfile.close(); 
-                //add(StationID,StationName,Status,Cost);
+            stList->add(StationID,StationName,Status,Cost);
+        }
+        else if(choice == '2'){
+            int index, data;
+            int size = stList->size();
+
+            if(size == 0){
+                cout << "cannot add station" << endl;
             }
-        void AddStation_Ontheway(){
-            string Stationid,index,Cost;
-                cout <<"Please Enter station id (index) : ";
-                cin >>index;
+            else{
+               string Cost;
                 cout << "Station ID: ";
-                cin >> Stationid;
-                    while(Stationid.length() != 5){
-                        cout << "!!! Warring !!!\n";
+                cin >> StationID;
+                    while(StationID.length() != 5){
+                        cout << "!!! Warning !!!\n";
                         cout << "Station ID Not equal to 5 ";
-                        cin >> Stationid;
+                        cin >> StationID;
                     }
                 cout << "StationName: ";
                 cin >> StationName;
+				if(stList->checkStationID(StationID)==true && stList->checkStationName(StationName)==true){
+					cout << "The station already exists!!!" << endl;
+				}
                 cout << "Station Status: ";
                 cin >> Status;
                 cout << "Station cost: ";
                 cin >> Cost;
-                ofstream Stationfile;
-                Stationfile.open("Station.txt",std::ios::app);
-                Stationfile << Stationid <<","<< StationName << "," << Status  << "," << Cost << endl;
-                Stationfile.close(); 
-                AddOntheWay(Stationid,StationName,Status,Cost);
-        }    
-};
-    /*
-int main(){
-    Station *st = new Station;
-    int addstationChoice,menu;
-	string Departure,Destination,On_theWay;
-    Addstation:cout <<"======== Add station ======== "<<endl;
-	cout <<"1.Add destination"<<endl;
-	cout <<"2.Add Departure station,Add station on the way"<<endl;
-	cout <<"3.Exit "<<endl;
-	cout <<"Enter ==> ";
-	cin >>addstationChoice;
-	switch(addstationChoice){
-		case 1:
-			cout <<"1.Add Destination station"<<endl;
-            st->AddStation();
-			cout <<"Add Destination station complete."<<endl;
-			goto Addstation;
-			break;		
-		case 2:
-            cout <<"Enter ==> ";
-	        cin >>menu;
-            switch(menu){
-            case 1:
-            cout <<"1.Add station on the way"<<endl;
-            st->AddStation_Ontheway();
-			cout <<"Add Station on the way station complete."<<endl;
-			goto Addstation;
-		    break;
-            case 2:
-            cout <<"2.Add Departure station"<<endl;
-            st->AddStation_Ontheway();
-            cout <<"Add Departure station complete."<<endl;
-            goto Addstation;
-		    break;   
+                if(size == 1){
+                    cout << "input position (1): ";
+                    cin >> index;
+                    if(index > size || index < 1){
+                       cout << "cannot add station" << endl;
+                    }
+                    else{
+                        stList->add(index,StationID,StationName,Status,Cost);
+                    }
+                }
+                else{
+                    cout << "choose position station(1 - " << size << "): ";
+                    cin >> index;
+
+                    
+                    if(index > size || index < 1){
+                    	cout << "cannot add station" << endl;
+					}
+					else{
+						stList->add(index,StationID,StationName,Status,Cost);
+					}
+                }
             }
-		case 3:
-			cout <<"Exit"<<endl;
-			goto EXIT;
-			break;
-	}
-    EXIT:cout <<"================="<<endl; 
-    return 0;
+        }
+
+    	else if(choice == '3'){
+		stList->show();
+    	stList->writefile();
+
 }
-*/
+        //system("cls");
+    }while(choice != '4');
+	//goto Addstation;
+	return 0;
+}
