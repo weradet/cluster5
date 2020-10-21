adde7a37f45bd7094c855dd47f3d8ece2c0a238c
weradet <70476442+weradet@users.noreply.github.com>
2020-10-21 22:31:11 +0700

    Update TopupCard.cpp

--- TopupCard.cpp
+++ TopupCard.cpp
@@ -1,6 +1,9 @@
 #include"TopupCard.h"
 TopupCardController::TopupCardController(){
-             list_member=new ListMember;
+             //list_member=new ListMember;
+             head = NULL;
+             tail = NULL;
+             Loaddata();
 }
 void TopupCardController::ShowMenuTopupCard(){
    
@@ -22,22 +25,74 @@ void TopupCardController::ShowMemberInformation(){
 }//show InforMember
 
 void TopupCardController::TopupMoney(double money,string password){
-    Member *customer = list_member->getMember(password);
-        if(customer!=NULL){
-            customer->Topupmoney(money);
-                ShowRemainingAmount(password);
-        }else{
-            cout << "Error Information!!" << endl;
+    Member *cur = head;
+    while(cur!=NULL){
+        if(cur->getPassword()==password){
+            cur->Topupmoney(money);
+            ShowRemainingAmount(password);
+            savedata();
+             break; 
         }
+         cur = cur->link;
+    }
 }//void topupMoney
 
 void TopupCardController::ShowRemainingAmount(string password){
-    Member *customer = list_member->getMember(password);
-    if(customer!=NULL){
-        cout << "Remaining Amount : " << customer->getMoney();
-        cout << "\n=====================================================" << endl;   
-    }else{
-        cout << "Error Information!!" << endl;
+      Member *cur = head;
+    while(cur!=NULL){
+        if(cur->getPassword()==password){
+             cout << "Remaining Amount : " << cur->getMoney();
+             cout << "\n=====================================================" << endl;  
+             break; 
+        }
+         cur = cur->link;
     }
 }//void showamoung
+
+void TopupCardController::SetMemberIndata(string firstname,string lastname,string tel,string password,string expire,string money){
+             Member *new_member = new Member(firstname,lastname,tel,password,expire,money);
+                 if(head == NULL){
+                    head = new_member;
+                    tail = new_member; 
+                  }else{
+                   tail->link = new_member;
+                   tail = new_member;
+                }  
+        }
+
+void TopupCardController:: savedata(){
+          Member *cur = head;
+          ofstream file("member.txt",ios::out);
+          if(file.is_open()){
+            while (cur!=NULL){ 
+              file<< cur->getFirstname() <<","<<cur->getLastname()<< "," << cur->getTel() << "," 
+              << cur->getPassword() << ","<< cur->getExpire() << "," << cur->getMoney()<< endl;   
+              cur=cur->link;
+            }//while
+          }//if
+        }
  
+void TopupCardController::Loaddata(){
+          string line,firstname,lastname,tel,password,expire,money;
+          ifstream data("member.txt",ios::in);
+          if(data.is_open()){
+            while(getline(data,line)){ 
+              firstname = line.substr(0,line.find(','));
+                          line.erase(0,line.find(',')+1); 
+              lastname = line.substr(0,line.find(','));
+                         line.erase(0,line.find(',')+1); 
+              tel = line.substr(0,line.find(','));
+                    line.erase(0,line.find(',')+1); 
+              password = line.substr(0,line.find(','));
+                         line.erase(0,line.find(',')+1); 
+              expire = line.substr(0,line.find(','));
+                       line.erase(0,line.find(',')+1); 
+              money = line.substr(0,line.find(','));
+                      line.erase(0,line.find(',')+1); 
+            SetMemberIndata(firstname,lastname,tel,password,expire,money);
+            }//while
+          }else{
+            cout << "File Error!!" << endl;
+          }
+          data.close();
+        }
