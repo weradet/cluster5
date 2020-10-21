8df54a8e7a73223fd3bc678871b5b57ee905191b
weradet <70476442+weradet@users.noreply.github.com>
2020-10-21 15:15:48 +0700

    Update TopupCard.cpp

--- TopupCard.cpp
+++ TopupCard.cpp
@@ -1,14 +1,12 @@
 #include"TopupCard.h"
 void TopupCardController::ShowMenuTopupCard(){
     int chioceMenu;
-        do{
             cout << "=============== TOPUP ==============="<< endl;
             cout << "1. Topup" << endl;
             cout << "2. Show balance" << endl;
             cout << "3. Back to menu" << endl;
             cout << "====================================="<< endl;
             cout << "" << endl;
-        }while(chioceMenu!=3);
                 
 }//void showMenu
 
@@ -21,7 +19,7 @@ void TopupCardController::ShowMemberInformation(){
 }//show InforMember
 
 void TopupCardController::TopupMoney(double money,string password){
-    Member *customer = list_member->getmember(password);
+    Member *customer = list_member->getMember(password);
         if(customer!=NULL){
             customer->Topupmoney(money);
                 ShowRemainingAmount(password);
@@ -31,7 +29,7 @@ void TopupCardController::TopupMoney(double money,string password){
 }//void topupMoney
 
 void TopupCardController::ShowRemainingAmount(string password){
-    Member *customer = list_member->getmember(password);
+    Member *customer = list_member->getMember(password);
     if(customer!=NULL){
         cout << "Remaining Amount : " << customer->getMoney();
         cout << "\n=====================================================" << endl;   
