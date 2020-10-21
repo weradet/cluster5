052c8bd9dcbc5599f149125b029c71d06f9b5bfb
wachiravit44 <garfieldandfield@gmail.com>
2020-10-21 13:57:25 +0700

    Create Status.h

---
+++ Status.h
@@ -0,0 +1,15 @@
+#ifndef STATUS_H
+#define STATUS_H
+#include"include.h"
+#include"Path.h"
+class Status :public Path{
+    public:
+        void ShowAllStation();
+        void showStation(string id);
+        void ChooseFixMenu(string id,int menu);
+        void FixCostStation(string id,int cost);
+        void Maintenance(string id);
+        void NewStationName(string id,string name);
+
+};
+#endif
