5f1dc2b4b387c0130cf82d85011d663fde4e60fb
weradet <70476442+weradet@users.noreply.github.com>
2020-10-21 15:54:28 +0700

    Update Ui.cpp

--- Ui.cpp
+++ Ui.cpp
@@ -4,42 +4,38 @@
             Regis_obj = new RegisterController;
      //       a = new Buy_ticket_controller;
           station_obj = new StationController;
-      //    topupcard = new TopupCardController;
+          topupcard = new TopupCardController;
        }
        void UI::PrintCustomer(){
             //print customer menu
-            system ("cls");
             cout << "************** Electic Train **************" << endl;
-            cout << "*  1. Find cycle Time                     *" << endl; 
-            cout << "*  2. Buy Train Ticket                    *" << endl;
-            cout << "*  3. Have a member                       *" << endl;
-            cout << "*  4. Back to Main menu                   *" << endl;
+            cout << "1. Find cycle Time" << endl; 
+            cout << "2. Buy Train Ticket" << endl;
+            cout << "3. Have a member" << endl;
+            cout << "4. Back to the main menu" << endl;
             cout << "*******************************************" << endl;
             cout << "Please Choose 1 Choice (1-4) : ";
        }//printcustomer menu
        void UI::PrintAdmin(){
            //print employee menu
            //print interface
-            system ("cls");
             cout << "************** Electic Train **************" << endl;
-            cout << "*  1. Find cycle Time                     *" << endl; 
-            cout << "*  2. Buy Train Ticket                    *" << endl;
-            cout << "*  3. Register Member                     *" << endl;
-            cout << "*  4. Renew Member Card                   *" << endl;
-            cout << "*  5. Add Station                         *" << endl;
-            cout << "*  6. Fix Station                         *" << endl;
-            cout << "*  7. Back to Main menu                   *" << endl;
+            cout << "1. Find cycle Time" << endl; 
+            cout << "2. Buy Train Ticket" << endl;
+            cout << "3. Register Member" << endl;
+            cout << "4. Renew Member Card" << endl;
+            cout << "5. Add Station" << endl;
+            cout << "6. Fix Station" << endl;
+            cout << "7. Back" << endl;
             cout << "*******************************************" << endl;
             cout << "Please Choose 1 Choice (1-7) : ";
        } // print Admin
 
        void UI::PrintFirstMenu(){
         //print the first menu
-        cout << "************* Personal **************" << endl;
-        cout << "*  1.Customer                       *" << endl;
-        cout << "*  2.Admin                          *" << endl;
-        cout << "*  3.Out                            *" << endl;
-        cout << "*************************************" << endl;
+        cout << "1.Customer" << endl;
+        cout << "2.Admin" << endl;
+        cout << "3.Out" << endl;
         cout << "Please Choose 1 Choice (1-3) : ";
         }
         void UI::ChooseFixMenu(string id,int menu){
@@ -64,7 +60,7 @@
              a->Departure_Staion("Departure");
         }*/
         void UI::InputMoney(double money,string pass){
-            Regis_obj->TopupMoney(money,pass);
+            topupcard->TopupMoney(money,pass);
             //Regis_obj->list_member->printlist();
         }
         void UI::savefile(){
