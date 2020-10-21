bae63b1ecb3909e3e542878464166e722cd0858f
weradet <70476442+weradet@users.noreply.github.com>
2020-10-21 22:31:05 +0700

    Update Register.cpp

--- Register.cpp
+++ Register.cpp
@@ -1,56 +1,100 @@
   #include"Register.h"
   RegisterController ::RegisterController(){
-            list_member = new ListMember;
+            //list_member = new ListMember;
+            head = NULL;
+            tail=NULL;
+            Loaddata();
         } 
         RegisterController::~RegisterController(){
         }//destructer
         void RegisterController :: SetMemberInformation(string f,string l,string t,string pw){
             //UserMember
-            UserMember.setMember(f,l,t,pw);  
-            list_member->Addmember(UserMember);   
-            //list_member->savedata();       
+            Member *new_member = new Member;
+            new_member->setMember(f,l,t,pw);  
+            if(head == NULL){
+            head = new_member;
+            tail = new_member; 
+          }else{
+            tail->link = new_member;
+            tail = new_member;
+          }       
         }
-        void RegisterController:: ShowUserInformation(string name){
-            Member *customer = list_member->searchMember(name);
-            if(customer != NULL){
+
+        void RegisterController:: ShowUserInformation(string name,string pass){
+            Member *customer = head;
+            while(customer != NULL){
+                 if(customer->getFirstname()==name&&customer->getPassword()==pass){
                 system ("cls");
                 cout << "============== Customer Information ==============" << endl;
                 cout << "Firstname : " << customer->getFirstname() << endl;
                 cout << "Lastname : " << customer->getLastname() << endl;
                 cout << "Tel. : " << customer->getTel() << endl;
                 //cout << "Password : " << customer->getPassword() << endl;
-            }else{
-                cout << "Information Error!!" << endl;
-            }    
+              }
+              customer = customer->link;
+            }
+              
         }
-        void RegisterController:: ShowExpireCard(string name){
-            Member *customer = list_member->searchMember(name);
-            //list_member->printlist();
-            if(customer != NULL){
-                cout << "ExpireCard : " << customer->getExpire() << endl; 
-            }else{
-                cout << "Information Error!!" << endl;
+        void RegisterController:: ShowExpireCard(string name,string pass){
+            Member *customer = head;
+            while(customer != NULL){
+              if(customer->getFirstname()==name&&customer->getPassword()==pass){
+                    cout << "ExpireCard : " << customer->getExpire() << endl;     
+              }
+              customer = customer->link;
             }
         }
-        void RegisterController:: TopupMoney(double money,string password){
-            Member *customer = list_member->getMember(password);
-            //cout <<"2 "<< password << endl;
-            //list_member->printlist();
-            //list_member->Loaddata();
-            if(customer != NULL){
-                customer->Topupmoney(money);
-                ShowRemainingAmount(password);
-                // list_member->Loaddata();
-            }else{
-                cout << "Information Error!!" << endl;
-            }
+        void RegisterController::SetMemberIndata(string firstname,string lastname,string tel,string password,string expire,string money){
+             Member *new_member = new Member(firstname,lastname,tel,password,expire,money);
+                 if(head == NULL){
+                    head = new_member;
+                    tail = new_member; 
+                  }else{
+                   tail->link = new_member;
+                   tail = new_member;
+                }  
+        }
+         void RegisterController::savedata(){
+          Member *cur = head;
+          ofstream file("member.txt",ios::out);
+          if(file.is_open()){
+            while (cur!=NULL){ 
+              file<< cur->getFirstname() <<","<<cur->getLastname()<< "," << cur->getTel() << "," 
+              << cur->getPassword() << ","<< cur->getExpire() << "," << cur->getMoney()<< endl;   
+              cur=cur->link;
+            }//while
+          }//if
+        }
+
+        void RegisterController::Loaddata(){
+          string line,firstname,lastname,tel,password,expire,money;
+          ifstream data("member.txt",ios::in);
+          if(data.is_open()){
+            while(getline(data,line)){ 
+              firstname = line.substr(0,line.find(','));
+                          line.erase(0,line.find(',')+1); 
+              lastname = line.substr(0,line.find(','));
+                         line.erase(0,line.find(',')+1); 
+              tel = line.substr(0,line.find(','));
+                    line.erase(0,line.find(',')+1); 
+              password = line.substr(0,line.find(','));
+                         line.erase(0,line.find(',')+1); 
+              expire = line.substr(0,line.find(','));
+                       line.erase(0,line.find(',')+1); 
+              money = line.substr(0,line.find(','));
+                      line.erase(0,line.find(',')+1); 
+            SetMemberIndata(firstname,lastname,tel,password,expire,money);
+            }//while
+          }else{
+            cout << "File Error!!" << endl;
+          }
+          data.close();
+        }
+        void RegisterController::Showallmember(){
+                   Member *cur = head;
+                   while(cur != NULL){
+                         cout << cur->getFirstname() << " " << cur->getLastname() 
+                         << " " << cur->getMoney() << " " << cur->getPassword() << endl;
+                         cur = cur->link; 
+                   }
         } 
-        void RegisterController:: ShowRemainingAmount(string password){
-            Member *customer = list_member->getMember(password);
-            if(customer != NULL){
-                cout << "Remaining Amount : " << customer->getMoney();
-                cout << "\n==================================================" << endl;   
-            }else{
-                cout << "Information Error!!" << endl;
-            }
-        }
