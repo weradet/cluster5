d1ecc27f45f9b5307ba6bc2f4eac86cf13368021
wachiravit44 <garfieldandfield@gmail.com>
2020-10-21 17:02:22 +0700

    Create Renewcard.cpp

---
+++ Renewcard.cpp
@@ -0,0 +1,13 @@
+ #include "Renewcard.h"
+ void RenewController::PrintRenewMenu(){
+        //print interface ewnew card menu
+        cout << "****Renew Card****" << endl;
+        cout << "1.Renew Card" << endl;
+        cout << "2. Back " << endl;
+    }
+    void RenewController::Entercardcode(string cardcode){
+
+    }
+    bool RenewController::Findmember(){
+       return true; 
+    }
