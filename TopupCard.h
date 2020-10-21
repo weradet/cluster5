e3f557b86ce3cd41fd24c6e86d4580b7d4d13ba2
wachiravit44 <garfieldandfield@gmail.com>
2020-10-21 14:38:52 +0700

    Update TopupCard.h

--- TopupCard.h
+++ TopupCard.h
@@ -1,4 +1,6 @@
 //62160157 Preechaya Choosrithong
+#ifndef TOPUPCARD_H
+#define TOPUPCARD_H
 #include"include.h"
 #include"domainclass.h"
 class TopupCardController{
@@ -7,45 +9,11 @@ class TopupCardController{
             ListMember *list_member;
         public:
 
-            void ShowMenuTopupCard(){
-                int chioceMenu;
-                do{
-                    cout << "=============== TOPUP ==============="<< endl;
-                    cout << "1. Topup" << endl;
-                    cout << "2. Show balance" << endl;
-                    cout << "3. Back to menu" << endl;
-                    cout << "====================================="<< endl;
-                    cout << "" << endl;
-                }while(chioceMenu!=3);
-                
-            }
-
-            void CheckMember(string Pass_Card){
-                
-                
-            }
-
-            void ShowMemberInformation(){
-
-            }
-
-            void TopupMoney(double money,string password){
-                 Member *customer = list_member->getmember(password);
-                 if(customer!=NULL){
-                      customer->Topupmoney(money);
-                      ShowRemainingAmount(password);
-                 }else{
-                    cout << "Error Information!!" << endl;
-                  }
-            }
-        void ShowRemainingAmount(string password){
-            Member *customer = list_member->getmember(password);
-            if(customer!=NULL){
-                cout << "Remaining Amount : " << customer->getMoney();
-                cout << "\n=====================================================" << endl;   
-            }else{
-                cout << "Error Information!!" << endl;
-            }
-        }
+        void ShowMenuTopupCard();
+        void CheckMember(string Pass_Card);
+        void ShowMemberInformation();
+        void TopupMoney(double money,string password);
+        void ShowRemainingAmount(string password);
  
 };
+ #endif           
