ee85db052a33b1f5b0c40aae2f44d534a3c33109
weradet <70476442+weradet@users.noreply.github.com>
2020-10-22 02:49:50 +0700

    Update Renewcard.cpp

--- Renewcard.cpp
+++ Renewcard.cpp
@@ -1,5 +1,11 @@
  #include "Renewcard.h"
- void RenewController::PrintRenewMenu(){
+
+   RenewController::RenewController(){
+    head = NULL;
+    tail = NULL;
+   }
+
+   void RenewController::PrintRenewMenu(){
         //print interface ewnew card menu
         cout << "****Renew Card****" << endl;
         cout << "1.Renew Card" << endl;
@@ -8,6 +14,72 @@
     void RenewController::Entercardcode(string cardcode){
 
     }
-    bool RenewController::Findmember(){
-       return true; 
+   void RenewController::RenewCard(Member *cur,int comit){
+       string temp = cur->getExpire();
+       string year = temp.substr(0,temp.find('-'));
+                     temp.erase(0,temp.find('-')+1);
+       string month = temp.substr(0,temp.find('-'));
+                     temp.erase(0,temp.find('-')+1);
+       string day = temp.substr(0,temp.find('-'));
+                      temp.erase(0,temp.find('-')+1);            
+       cur->getmembercard().GenerateExpire();   
+       cout << cur->getExpire();                                  
+       //cur->
+   }
+   Member* RenewController::Findmember(string password){
+          Member *cur = head;
+          while(cur != NULL){
+             if(cur->getPassword()==password){
+                return cur;
+             }
+              cur = cur->link;
+          }
+          return NULL;
     }
+    void RenewController::SetMemberIndata(string firstname,string lastname,string tel,string password,string expire,string money){
+             Member *new_member = new Member(firstname,lastname,tel,password,expire,money);
+                 if(head == NULL){
+                    head = new_member;
+                    tail = new_member; 
+                  }else{
+                   tail->link = new_member;
+                   tail = new_member;
+                }  
+        }
+
+   void RenewController:: savedata(){
+          Member *cur = head;
+          ofstream file("member.txt",ios::out);
+          if(file.is_open()){
+            while (cur!=NULL){
+              file<< cur->getFirstname() <<","<<cur->getLastname()<< "," << cur->getTel() << "," 
+              << cur->getPassword() << ","<< cur->getExpire() << "," << cur->getMoney()<< endl;   
+              cur=cur->link;
+            }//while
+          }//if
+        }
+ 
+   void RenewController::Loaddata(){
+          string line,firstname,lastname,tel,password,expire,money;
+          ifstream data("member.txt",ios::in);
+          if(data.is_open()){
+            while(getline(data,line)){ 
+              firstname = line.substr(0,line.find(','));
+                          line.erase(0,line.find(',')+1); 
+              lastname = line.substr(0,line.find(','));
+                         line.erase(0,line.find(',')+1); 
+              tel = line.substr(0,line.find(','));
+                    line.erase(0,line.find(',')+1); 
+              password = line.substr(0,line.find(','));
+                         line.erase(0,line.find(',')+1); 
+              expire = line.substr(0,line.find(','));
+                       line.erase(0,line.find(',')+1); 
+              money = line.substr(0,line.find(','));
+                      line.erase(0,line.find(',')+1); 
+            SetMemberIndata(firstname,lastname,tel,password,expire,money);
+            }//while
+          }else{
+            cout << "File Error!!" << endl;
+          }
+          data.close();
+        }
