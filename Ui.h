52c5f21e2a80467ae38efedd0c3c7bb533f706b3
weradet <70476442+weradet@users.noreply.github.com>
2020-10-21 14:12:44 +0700

    Update Ui.h

--- Ui.h
+++ Ui.h
@@ -1,5 +1,7 @@
+#ifndef UI_H
+#define UI_H
 #include"include.h"
-#include"station.h"
+//#include"station.h"
 //#include"domainclass.h"
 #include"Register.h"
 #include"Renewcard.h"
@@ -13,84 +15,21 @@ class UI{
    RenewController *Renew_obj;  
    RegisterController *Regis_obj;
    //Buy_ticket_controller *a;
-    StationController *station_obj;
+    //StationController *station_obj;
     //TopupCardController *topupcard;
     public:
-       UI(){
-            Renew_obj = new RenewController;
-            Regis_obj = new RegisterController;
-     //       a = new Buy_ticket_controller;
-          station_obj = new StationController;
-      //    topupcard = new TopupCardController;
-       }
-       void PrintCustomer(){
-            //print customer menu
-            cout << "************** Electic Train **************" << endl;
-            cout << "1. Find cycle Time" << endl; 
-            cout << "2. Buy Train Ticket" << endl;
-            cout << "3. Have a member" << endl;
-            cout << "4. Back to the main menu" << endl;
-            cout << "*******************************************" << endl;
-            cout << "Please Choose 1 Choice (1-4) : ";
-       }//printcustomer menu
-       void PrintAdmin(){
-           //print employee menu
-           //print interface
-            cout << "************** Electic Train **************" << endl;
-            cout << "1. Find cycle Time" << endl; 
-            cout << "2. Buy Train Ticket" << endl;
-            cout << "3. Register Member" << endl;
-            cout << "4. Renew Member Card" << endl;
-            cout << "5. Add Station" << endl;
-            cout << "6. Fix Station" << endl;
-            cout << "7. Back" << endl;
-            cout << "*******************************************" << endl;
-            cout << "Please Choose 1 Choice (1-7) : ";
-       } // print Admin
-
-       void PrintFirstMenu(){
-        //print the first menu
-        cout << "1.Customer" << endl;
-        cout << "2.Admin" << endl;
-        cout << "3.Out" << endl;
-        cout << "Please Choose 1 Choice (1-3) : ";
-        }//print FirstMenu
-      /*void CheckMenuAdmin(int menu){
-            //check is menu admin in the method PrintAdmin() in UI class
-            if(menu == 4){
-            //Renew Card
-               cout << "Hello";   
-            }//if
-        }*/
-        void ChooseFixMenu(string id,int menu){
-            station_obj->ChooseFixMenu(id,menu);
-        }
-        void ShowFixMenuStation(){
-            station_obj->ShowFixMenuStation();
-        }
-        void ShowAllStation(){
-            station_obj->ShowAllStation();
-        }
-        void InputPersonalInformation(string firstname,string lastname,string tel,string password){
-            Regis_obj->SetMemberInformation(firstname,lastname,tel,password);   
-        }//inputpersonal
-        void ShowExprirationDate(string name){
-            Regis_obj->ShowExpireCard(name);
-        }
-        void ShowInformation(string name){
-            Regis_obj->ShowUserInformation(name);
-        }
-       /* void printfile(){
-             a->Departure_Staion("Departure");
-        }*/
-        void InputMoney(double money,string pass){
-            Regis_obj->TopupMoney(money,pass);
-            //Regis_obj->list_member->printlist();
-        }
-        void savefile(){
-            Regis_obj->list_member->savedata();
-        }
-        void loadfile(){
-            Regis_obj->list_member->Loaddata();
-        }
+       UI();
+       void PrintCustomer();
+       void PrintAdmin();
+        void PrintFirstMenu();
+        void ChooseFixMenu(string id,int menu);
+        void ShowFixMenuStation();
+        void ShowAllStation();
+        void InputPersonalInformation(string firstname,string lastname,string tel,string password);
+        void ShowExprirationDate(string name);
+        void ShowInformation(string name);
+        void InputMoney(double money,string pass);
+        void savefile();
+        void loadfile();
 };
+#endif
