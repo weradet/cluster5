130acf4d70b4675befc2e88b7c8b04eff510eea2
preechaya157 <peechaya72@gmail.com>
2020-10-21 15:07:27 +0700

    Update TopupCard.cpp

--- TopupCard.cpp
+++ TopupCard.cpp
@@ -1,41 +1,42 @@
 #include"TopupCard.h"
 void TopupCardController::ShowMenuTopupCard(){
-                int chioceMenu;
-                do{
-                    cout << "=============== TOPUP ==============="<< endl;
-                    cout << "1. Topup" << endl;
-                    cout << "2. Show balance" << endl;
-                    cout << "3. Back to menu" << endl;
-                    cout << "====================================="<< endl;
-                    cout << "" << endl;
-                }while(chioceMenu!=3);
+    int chioceMenu;
+        do{
+            cout << "=============== TOPUP ==============="<< endl;
+            cout << "1. Topup" << endl;
+            cout << "2. Show balance" << endl;
+            cout << "3. Back to menu" << endl;
+            cout << "====================================="<< endl;
+            cout << "" << endl;
+        }while(chioceMenu!=3);
                 
-            }
-            void TopupCardController::CheckMember(string Pass_Card){
-                
-                
-            }
+}//void showMenu
 
-            void TopupCardController::ShowMemberInformation(){
+void TopupCardController::CheckMember(string Pass_Card){
 
-            }
+}//voidCheck
 
-            void TopupCardController::TopupMoney(double money,string password){
-                 Member *customer = list_member->getMember(password);
+void TopupCardController::ShowMemberInformation(){
+
+}//show InforMember
+
+void TopupCardController::TopupMoney(double money,string password){
+                 Member *customer = list_member->getmember(password);
                  if(customer!=NULL){
                       customer->Topupmoney(money);
                       ShowRemainingAmount(password);
                  }else{
                     cout << "Error Information!!" << endl;
                   }
-            }
-        void TopupCardController::ShowRemainingAmount(string password){
-            Member *customer = list_member->getMember(password);
-            if(customer!=NULL){
-                cout << "Remaining Amount : " << customer->getMoney();
-                cout << "\n=====================================================" << endl;   
-            }else{
-                cout << "Error Information!!" << endl;
-            }
-        }
+}//void topupMoney
+
+void TopupCardController::ShowRemainingAmount(string password){
+    Member *customer = list_member->getmember(password);
+    if(customer!=NULL){
+        cout << "Remaining Amount : " << customer->getMoney();
+        cout << "\n=====================================================" << endl;   
+    }else{
+        cout << "Error Information!!" << endl;
+    }
+}//void showamoung
  
