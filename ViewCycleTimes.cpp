1fd2456b7af8f4137f743e10ade0e2cad6e59218
wachiravit44 <garfieldandfield@gmail.com>
2020-10-22 17:41:58 +0700

    Update ViewCycleTimes.cpp

--- ViewCycleTimes.cpp
+++ ViewCycleTimes.cpp
@@ -3,24 +3,58 @@
             head = new Path;
             head->read_file();
         }
-        void ViewCycleTime::ChooseDepartureStation(string name){
+        void ViewCycleTime::ChooseDepartureStation(){
             Path *a = head;
-         //   Path *b = head;
+                HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE);
+                for(int i=0;i<a->count;i++){
+                    if(a->head->stationStatus() != "close"){
+                        
+                        SetConsoleTextAttribute(color,5);
+                        cout <<"ID DepartureStation: ";
+                        SetConsoleTextAttribute(color,6);
+                        cout << a->head->StationID();
+                        SetConsoleTextAttribute(color,5);
+                        cout << " Station Name: " ;
+                        SetConsoleTextAttribute(color,6);
+                        cout <<a->head->stationName() <<endl;
+                        
+                    }
+                    a->head = a->head->link;
+                }
+                SetConsoleTextAttribute(color,7);
+                cout << "Enter ID TerminalStation: ";
+                SetConsoleTextAttribute(color,14);
+                cin >> DepartureStation;
+                SetConsoleTextAttribute(color,7);
+        }
+
+        void ViewCycleTime::ChooseTerminalStation(){
+            Path *a = head;
                 while(a->head != NULL){
-                    if(a->head->stationName()== name){
+                    if(a->head->StationID()== DepartureStation){
                         break;
                     }
                     a->head=a->head->link;
                 }
                 a->head=a->head->link;
+                HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE);
                 for(int i=0;i<a->count-1;i++){
                     if(a->head->stationStatus() != "close"){
-                        cout << a->head->StationID()<<endl;
+                        
+                        SetConsoleTextAttribute(color,5);
+                        cout <<"ID TerminalStation: ";
+                        SetConsoleTextAttribute(color,6);
+                        cout << a->head->StationID();
+                        SetConsoleTextAttribute(color,5);
+                        cout << " Station Name: " ;
+                        SetConsoleTextAttribute(color,6);
+                        cout <<a->head->stationName() <<endl;
+                        
                     }
                     a->head = a->head->link;
                 }
-        }
-
-        void ViewCycleTime::ChooseTerminalStation(string name){
-
+                SetConsoleTextAttribute(color,7);
+                cout << "Enter ID TerminalStation: ";
+                SetConsoleTextAttribute(color,14);
+                cin >> TerminalStation;
         }
