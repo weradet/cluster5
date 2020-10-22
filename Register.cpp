64bd61737343290749cf98974214ba275bbdce18
NSD-NK <62160085@go.buu.ac.th>
2020-10-22 15:53:17 +0700

    Update Register.cpp

--- Register.cpp
+++ Register.cpp
@@ -1,72 +1,67 @@
-  #include"Register.h"
-  RegisterController ::RegisterController(){
-            //list_member = new ListMember;
-            head = NULL;
-            tail=NULL;
-            //Loaddata();
-        } 
-        RegisterController::~RegisterController(){
-        }//destructer
-        void RegisterController :: SetMemberInformation(string f,string l,string t,string pw){
-            //UserMember
-            Member *new_member = new Member;
-            new_member->setMember(f,l,t,pw);  
-            if(head == NULL){
-            head = new_member;
-            tail = new_member; 
-          }else{
-            tail->link = new_member;
-            tail = new_member;
-          }       
-        }
+#include"Register.h"
+  RegisterController::RegisterController(){
+    head = NULL;
+    tail = NULL;
+  }
+  RegisterController::~RegisterController(){
+  }//destructer
+  void RegisterController::SetMemberInformation(string f,string l,string t,string pw){
+    Member *new_member = new Member;
+    new_member->setMember(f,l,t,pw);  
+    if(head == NULL){
+      head = new_member;
+      tail = new_member; 
+    }else{
+      tail->link = new_member;
+      tail = new_member;
+    }       
+  }
+  void RegisterController::ShowUserInformation(string name,string pass){
+    Member *customer = head;
+    while(customer != NULL){
+      if(customer->getFirstname()==name&&customer->getPassword()==pass){
+        system ("cls");
+        cout << "============== Customer Information ==============" << endl;
+        cout << "Firstname : " << customer->getFirstname() << endl;
+        cout << "Lastname : " << customer->getLastname() << endl;
+        cout << "Tel. : " << customer->getTel() << endl;
+        //cout << "Password : " << customer->getPassword() << endl;
+      }
+      customer = customer->link;
+    }
+  }
+  void RegisterController:: ShowExpireCard(string name,string pass){
+    Member *customer = head;
+    while(customer != NULL){
+      if(customer->getFirstname()==name&&customer->getPassword()==pass){
+        cout << "ExpireCard : " << customer->getExpire() << endl;     
+      }
+      customer = customer->link;
+    }
+  }
+  void RegisterController::SetMemberInData(string firstname,string lastname,string tel,string password,string expire,string money){
+    Member *new_member = new Member(firstname,lastname,tel,password,expire,money);
+    if(head == NULL){
+      head = new_member;
+      tail = new_member; 
+    }else{
+      tail->link = new_member;
+      tail = new_member;
+    }  
+  }
+  void RegisterController::SaveData(){
+    Member *cur = head;
+    ofstream file("member.txt",ios::app);
+    if(file.is_open()){
+      while (cur!=NULL){ 
+        file << cur->getFirstname() <<","<<cur->getLastname()<< "," << cur->getTel() << "," 
+            << cur->getPassword() << ","<< cur->getExpire() << "," << cur->getMoney()<< endl;   
+        cur=cur->link;
+      }//while
+    }
+  }
 
-        void RegisterController:: ShowUserInformation(string name,string pass){
-            Member *customer = head;
-            while(customer != NULL){
-                 if(customer->getFirstname()==name&&customer->getPassword()==pass){
-                system ("cls");
-                cout << "============== Customer Information ==============" << endl;
-                cout << "Firstname : " << customer->getFirstname() << endl;
-                cout << "Lastname : " << customer->getLastname() << endl;
-                cout << "Tel. : " << customer->getTel() << endl;
-                //cout << "Password : " << customer->getPassword() << endl;
-              }
-              customer = customer->link;
-            }
-              
-        }
-        void RegisterController:: ShowExpireCard(string name,string pass){
-            Member *customer = head;
-            while(customer != NULL){
-              if(customer->getFirstname()==name&&customer->getPassword()==pass){
-                    cout << "ExpireCard : " << customer->getExpire() << endl;     
-              }
-              customer = customer->link;
-            }
-        }
-        void RegisterController::SetMemberIndata(string firstname,string lastname,string tel,string password,string expire,string money){
-             Member *new_member = new Member(firstname,lastname,tel,password,expire,money);
-                 if(head == NULL){
-                    head = new_member;
-                    tail = new_member; 
-                  }else{
-                   tail->link = new_member;
-                   tail = new_member;
-                }  
-        }
-         void RegisterController::savedata(){
-          Member *cur = head;
-          ofstream file("member.txt",ios::app);
-          if(file.is_open()){
-            while (cur!=NULL){ 
-              file<< cur->getFirstname() <<","<<cur->getLastname()<< "," << cur->getTel() << "," 
-              << cur->getPassword() << ","<< cur->getExpire() << "," << cur->getMoney()<< endl;   
-              cur=cur->link;
-            }//while
-          }//if
-        }
-
-        void RegisterController::Loaddata(){
+        void RegisterController::LoadData(){
           string line,firstname,lastname,tel,password,expire,money;
           ifstream data("member.txt",ios::in);
           if(data.is_open()){
@@ -83,7 +78,7 @@
                        line.erase(0,line.find(',')+1); 
               money = line.substr(0,line.find(','));
                       line.erase(0,line.find(',')+1); 
-            SetMemberIndata(firstname,lastname,tel,password,expire,money);
+            SetMemberInData(firstname,lastname,tel,password,expire,money);
             }//while
           }else{
             cout << "File Error!!" << endl;
@@ -91,7 +86,7 @@
           data.close();
         }
         
-        void RegisterController::Showallmember(){
+        void RegisterController::ShowAllMember(){
                    Member *cur = head;
                    while(cur != NULL){
                          cout << cur->getFirstname() << " " << cur->getLastname() 
