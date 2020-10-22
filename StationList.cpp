0c87df18cc682f48acc2b3d74454a07d92df6f56
ธนิสร  ธรรมสวนิต <62160088@go.buu.ac.th>
2020-10-22 21:37:54 +0700

    Update StationList.cpp

--- StationList.cpp
+++ StationList.cpp
@@ -1,12 +1,39 @@
 #include"StationList.h"
-		void StationList::Addstation(string StationID,string StationName,string Status,int Cost){
-			Path::add_path(StationID,StationName,Status,Cost);	
+//#include "include.h"
+//#include"node_Addstation.h"
+		StationList::StationList(){
+			head = NULL;
+			tail = NULL;
+			count = 0;
 		}
-		void StationList::AddStation_ontheway(int index,string StationID,string StationName,string Status,int Cost){
-			Station *newnode = new Station(StationID,StationName,Status,Cost);
-			Station *temp = head;
+		StationList::~StationList(){
+			for(int i=1;i<count;i++){
+				Node_Addstation *tmp = head;
+				head = head->link;
+				delete tmp;
+				tmp = NULL;
+			}
+			head = NULL;
+			tail = NULL;
+		}
+		void StationList::Addstation(string StationName,string StationID,string Status,int Cost){
+			Node_Addstation *n = new Node_Addstation(StationName,StationID,Status,Cost);
+			if(head == NULL){
+				head = n;
+				tail = head;
+				count++;
+			}else{
+				tail->link = n;
+				n->plink = tail;	
+				tail = n;
+				count++;
+			}			
+		}
+		void StationList::AddStation_ontheway(int index,string StationName,string StationID,string Status,int Cost){
+			Node_Addstation *newnode = new Node_Addstation(StationName,StationID,Status,Cost);
+			Node_Addstation *temp = head;
 			if(index > count){
-				cout << "Overflow" << endl;
+				cout << "cannot add" << endl;
 			}else if(index == 1){				
 				newnode->link = head;
 				head->plink = newnode;
@@ -25,12 +52,34 @@
 					i++;
 				}
 				count++;
-			}				
+			}		
+		}
+		void StationList::ReadStationFile(){
+			string line;
+			string StationName,StationID,Status;
+			string C;
+			ifstream myFile("Station.txt", ios::in);
+				if(myFile.is_open()){ 
+					while(getline(myFile,line)){
+						StationName = line.substr(0,line.find(','));
+							line.erase(0,line.find(',')+1);	
+						StationID = line.substr(0,line.find(','));
+							line.erase(0,line.find(',')+1);
+						Status = line.substr(0,line.find(','));
+							line.erase(0,line.find(',')+1);
+						C = line.substr(0,line.find(','));
+							line.erase(0,line.find(',')+1);
+						stringstream COST(C);
+						int Cost = 0;
+						COST >> Cost;
+						Addstation(StationName,StationID,Status,Cost);
+					}
+				}
 		}
 		bool StationList::checkStationID(string id){
-			Station *temp = head;
+			Node_Addstation *temp = head;
 			while(temp !=NULL){
-				if(temp->StationID() == id){
+				if(temp->StationID == id){
 					return true;
 					break;
 				}
@@ -39,9 +88,9 @@
             return false;
 		}
 		bool StationList::checkStationName(string name){
-			Station *temp = head;
+			Node_Addstation *temp = head;
 			while(temp !=NULL){
-				if(temp->stationName() == name){
+				if(temp->StationName == name){
 					return true;
 					break;
 				}
@@ -50,21 +99,20 @@
             return true;
 		}
 		void StationList::show_station(){
-			Station *temp = head;
+			Node_Addstation *temp = head;
 			int i = 1;
 			while(temp != NULL){
-				cout << "Station" << i << ": = " << temp->StationID() << temp->stationName() << temp->stationStatus() << temp->cost() << endl;
+				cout << "Station" << i << ": = " << temp->StationID << temp->StationName << temp->Cost << temp->Status << endl;
 				temp = temp->link;
 				i++;
 			}		
 		}
 		void StationList::WriteStationfile(){
-			string line3;
-			Station *temp = head;
+			Node_Addstation *temp = head;
 			ofstream myFile3("Station.txt",ios::app);
         	if(myFile3.is_open()){ 
         	while(temp!=NULL){
-				myFile3  << temp->stationName() <<","<< temp->StationID() << "," << temp->cost()  << ","  << temp->stationStatus() << endl;
+				myFile3  << temp->StationName <<","<< temp->StationID << "," << temp->Cost  << ","  << temp->Status << endl;
 				temp = temp->link;
 				if(temp == head){
                 	break;
