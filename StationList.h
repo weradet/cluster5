bad12c5a5dd7b8467bbe363714fd06d69f03ea6c
ธนิสร  ธรรมสวนิต <62160088@go.buu.ac.th>
2020-10-22 21:37:45 +0700

    Update StationList.h

--- StationList.h
+++ StationList.h
@@ -1,11 +1,18 @@
 #ifndef STATIONLIST_H
 #define STATIONLIST_H
 #include"include.h"
-#include"Path.h"
-class StationList :public Path{
+#include"node_Addstation.h"
+class StationList{
+	private:		
+		Node_Addstation *head;
+		Node_Addstation *tail;
+		int count;
 	public:
+		StationList();
+		~StationList();
 		void Addstation(string StationID,string StationName,string Status,int Cost);
 		void AddStation_ontheway(int index,string StationID,string StationName,string Status,int Cost);
+		void ReadStationFile();
 		bool checkStationID(string id);
 		bool checkStationName(string name);
 		void show_station();
