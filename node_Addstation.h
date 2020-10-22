b27e096b3af0608da3b4088eb1d661f91d69ed74
ธนิสร  ธรรมสวนิต <62160088@go.buu.ac.th>
2020-10-22 21:37:23 +0700

    Create node_Addstation.h

---
+++ node_Addstation.h
@@ -0,0 +1,13 @@
+#ifndef NODE_ADDSTATION_H
+#define NODE_ADDSTATION_H
+#include"include.h"
+class Node_Addstation{
+	public:
+		string StationName,StationID,Status;
+		int Cost;
+		Node_Addstation *link;
+		Node_Addstation *plink;
+		Node_Addstation(string Sid,string name,string stat,int c);
+
+};
+#endif
