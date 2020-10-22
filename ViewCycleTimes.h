2c1e6582ec75b321d213e94e90b6852b263c732e
62160114 <72860516+62160114@users.noreply.github.com>
2020-10-22 18:32:38 +0700

    Update ViewCycleTimes.h

--- ViewCycleTimes.h
+++ ViewCycleTimes.h
@@ -3,16 +3,18 @@
 #include"include.h"
 #include"Path.h"
 #include"station.h"
+#include"Round.h"
 class ViewCycleTime{
     private :
         string DepartureStation;
         string TerminalStation;
         Station my_station;
         Path *head;
+        Round *round;
     public :
         ViewCycleTime(void);
         void ChooseDepartureStation();
         void ChooseTerminalStation();
-
+        void SearchRound();
 };
 #endif
