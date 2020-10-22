1bc81e92b4eb41f1ab11fddc5893cf3cf823fa29
NSD-NK <62160085@go.buu.ac.th>
2020-10-22 17:32:31 +0700

    Update Ui.cpp

--- Ui.cpp
+++ Ui.cpp
@@ -1,49 +1,42 @@
 #include"Ui.h"
-       UI::UI(){
+        UI::UI(){
             Renew_obj = new RenewController;
             Regis_obj = new RegisterController;
-     //       a = new Buy_ticket_controller;
             station_obj = new StationController;
-           // topupcard = new TopupCardController;
-            //node_Addstation_obj = ;
             StationList_obj = new StationList;
             view = new ViewCycleTime;
-       }
-       void UI::PrintCustomer(){
-            //print customer menu
+        }
+        void UI::PrintCustomer(){
             system ("cls");
-            cout << "************** Electic Train **************" << endl;
-            cout << "*  1. Find cycle Time                     *" << endl; 
-            cout << "*  2. Buy Train Ticket                    *" << endl;
-            cout << "*  3. Have a member                       *" << endl;
-            cout << "*  4. Back to Main menu                   *" << endl;
-            cout << "*******************************************" << endl;
-            cout << "Please Choose 1 Choice (1-4) : ";
-       }//printcustomer menu
-       void UI::PrintAdmin(){
-           //print employee menu
-           //print interface
+            cout << "\t\t************** Electic Train **************" << endl;
+            cout << "\t\t*  1. Find Cycle Time                     *" << endl; 
+            cout << "\t\t*  2. Buy Train Ticket                    *" << endl;
+            cout << "\t\t*  3. Topup The Card                      *" << endl;
+            cout << "\t\t*  4. Back To Menu                        *" << endl;
+            cout << "\t\t*******************************************" << endl;
+            cout << "\t\tPlease Choose 1 Choice (1-4) : ";
+        }//Print Menu For Customer
+        void UI::PrintAdmin(){
             //system ("cls");
-            cout << "************** Electic Train **************" << endl;
-            cout << "*  1. Find cycle Time                     *" << endl; 
-            cout << "*  2. Buy Train Ticket                    *" << endl;
-            cout << "*  3. Register Member                     *" << endl;
-            cout << "*  4. Renew Member Card                   *" << endl;
-            cout << "*  5. Add Station                         *" << endl;
-            cout << "*  6. Fix Station                         *" << endl;
-            cout << "*  7. Back to Main menu                   *" << endl;
-            cout << "*******************************************" << endl;
-            cout << "Please Choose 1 Choice (1-7) : ";
-       } // print Admin
-
-       void UI::PrintFirstMenu(){
-        //print the first menu
-        cout << "\t\t************* Personal *************" << endl;
-        cout << "\t\t*  1.Customer                      *" << endl;
-        cout << "\t\t*  2.Admin                         *" << endl;
-        cout << "\t\t*  3.Exit                          *" << endl;
-        cout << "\t\t************************************" << endl;
-        cout << "\t\tPlease Choose 1 Choice (1-3) : ";
+            cout << "\t\t************** Electic Train **************" << endl;
+            cout << "\t\t*  1. Find Cycle Time                     *" << endl; 
+            cout << "\t\t*  2. Buy Train Ticket                    *" << endl;
+            cout << "\t\t*  3. Register Member                     *" << endl;
+            cout << "\t\t*  4. Renew Member Card                   *" << endl;
+            cout << "\t\t*  5. Add Station                         *" << endl;
+            cout << "\t\t*  6. Fix Station                         *" << endl;
+            cout << "\t\t*  7. Back To Menu                        *" << endl;
+            cout << "\t\t*******************************************" << endl;
+            cout << "\t\tPlease Choose 1 Choice (1-7) : ";
+        }//Print Menu For Admin
+        void UI::PrintFirstMenu(){
+            //print the first menu
+            cout << "\t\t************* Personal *************" << endl;
+            cout << "\t\t*  1.CUSTOMER                      *" << endl;
+            cout << "\t\t*  2.ADMIN                         *" << endl;
+            cout << "\t\t*  3.EXIT THE PROGRAM              *" << endl;
+            cout << "\t\t************************************" << endl;
+            cout << "\t\tPlease Choose 1 Choice (1-3) : ";
         }
         void UI::readfile(){
             station_obj->readfile();
@@ -63,93 +56,82 @@
         void UI::ShowStaiton(string id){
             station_obj->ShowStation(id);
         }
-
-        /*Usecase Register and Topup Card*/
+        /* Usecase Register */
         void UI::InputPersonalInformation(string firstname,string lastname,string tel,string password){
             Regis_obj->SetMemberInformation(firstname,lastname,tel,password);   
-        }//inputpersonal
-        void UI::ShowExprirationDate(string name,string pass){
-            Regis_obj->ShowExpireCard(name,pass);
         }
-        void UI::ShowInformation(string name,string pass){
-            Regis_obj->ShowUserInformation(name,pass);
+        void UI::ShowInformation(string firstname,string password){
+            Regis_obj->ShowUserInformation(firstname,password);
         }
-        void UI::ShowAllMember(){
-            Regis_obj->ShowAllMember();
+        void UI::ShowExprirationDate(string firstname,string password){
+            Regis_obj->ShowExpireCard(firstname,password);
         }
-        void UI::InputMoney(double money,string pass){
-            //topupcard->Loaddata();
-            topupcard.TopupMoney(money,pass);
-            //Regis_obj->list_member->printlist();
-        }
-
-        void UI::InputMoneyCard(double numMoney,string password){
-            //topupcard->Loaddata();
-            Regis_obj->InputMoneyCard(numMoney,password);
-            //Regis_obj->list_member->printlist();
+        void UI::InputMoneyCard(double money,string password){
+            Regis_obj->InputMoneyCard(money,password);
         }
         void UI::LoadFile(){
-              Regis_obj->LoadData();
+            Regis_obj->LoadData();
         }
         void UI::SaveFile(){
-           // Regis_obj->list_member->savedata();
-              Regis_obj->SaveData();
+            Regis_obj->SaveData();
         }
-        void UI::loadtopupfile(){
-           // Regis_obj->list_member->savedata();
-              topupcard.Loaddata();
+        void UI::ShowMember(){
+            Regis_obj->ShowMember();
         }
-        void UI::savetopupfile(){
-           // Regis_obj->list_member->savedata();
-              topupcard.savedata();
+        /* Usecase Topup Card */
+        void UI::ShowMenu(){
+            topupcard.ShowMenuTopupCard();
         }
         bool UI::checkmember(string pass){
-             Member *member = topupcard.CheckMember(pass);
-             if(member!=NULL){
+            Member *member = topupcard.CheckMember(pass);
+            if(member!=NULL){
                 return true;
-             }
-                return false;
+            }
+            return false;
+        }
+        void UI::InputMoney(double money,string pass){
+            topupcard.TopupMoney(money,pass);
         }
         void UI::Showmoney(string password){
             Member *member = topupcard.CheckMember(password);
             topupcard.ShowRemainingAmount(member);
-        } 
-        void UI::ShowMenu(){
-          topupcard.ShowMenuTopupCard();
+        }
+        void UI::loadtopupfile(){
+            topupcard.Loaddata();
+        }
+        void UI::savetopupfile(){
+            topupcard.savedata();
         }
         void UI::Clear(){
             topupcard.clear();
         }
-
-         /*Usecase Renew Card */
+        /*Usecase Renew Card */
         void UI::showmenuRenewCard(){
             Renew_obj->PrintRenewMenu();
         } 
-       void UI::EnterCardcode(string password){
-              Member *member = Renew_obj->Findmember(password);  
-              if(member!=NULL){
-                  int comit;
-                  cout << "Name : " << member->getFirstname() << endl;
-                  cout << "LastName : " << member->getLastname() << endl;
-                  cout << "Confirm Renew ? (1.Renew,2.No)"; cin >> comit;
-                  if(!cin){
-                        cin.clear(); 
-                        cin.ignore(100, '\n');
-                  }
-                   Renew_obj->RenewCard(member,comit); 
-              }
-              else{
-                  cout << "Error Find !!" << endl;
-              }  
+        void UI::EnterCardcode(string password){
+            Member *member = Renew_obj->Findmember(password);  
+            if(member!=NULL){
+                int comit;
+                cout << "Name : " << member->getFirstname() << endl;
+                cout << "LastName : " << member->getLastname() << endl;
+                cout << "Confirm Renew ? (1.Renew,2.No)"; cin >> comit;
+                if(!cin){
+                    cin.clear(); 
+                    cin.ignore(100, '\n');
+                }
+                Renew_obj->RenewCard(member,comit); 
+            }else{
+                cout << "Error Find !!" << endl;
+            }  
         }
-        void  UI::loaddataRenew(){
-              Renew_obj->Loaddata();
+        void UI::loaddataRenew(){
+            Renew_obj->Loaddata();
         }
         void UI::SaveDataRenew(){
             Renew_obj->savedata();
         }
- 
-       /*Usecase Add Station */
+        /* Usecase Add Station */
         void UI::Addstation(string StationID,string StationName,string Status,int Cost){
             StationList_obj->Addstation(StationID,StationName,Status,Cost);
         }
@@ -170,8 +152,7 @@
         }
         int UI::StationNumber(){
             return StationList_obj->StationNumber();
-        }//
-
+        }
         void UI::View(){
             view->ChooseDepartureStation("Siam");
         }
