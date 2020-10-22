74dec47d222cf1b3f55916ab5a5de11d052401fa
62160150 <62160150@go.buu.ac.th>
2020-10-22 16:29:26 +0700

    Update StationList.h

--- StationList.h
+++ StationList.h
@@ -1,17 +1,11 @@
 #ifndef STATIONLIST_H
 #define STATIONLIST_H
 #include"include.h"
-#include"node_Addstation.h"
-class StationList{
-	private:		
-		Node_Addstation *head;
-		Node_Addstation *tail;
-		int count;
+#include"Path.h"
+class StationList :public Path{
 	public:
-		StationList();
-		~StationList();
-		void Addstation(string StationID,string StationName,string Status,string Cost);
-		void AddStation_ontheway(int index,string StationID,string StationName,string Status,string Cost);
+		void Addstation(string StationID,string StationName,string Status,int Cost);
+		void AddStation_ontheway(int index,string StationID,string StationName,string Status,int Cost);
 		bool checkStationID(string id);
 		bool checkStationName(string name);
 		void show_station();
