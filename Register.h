f3bb543a7d1d048534113e38469b4eecc598decf
NSD-NK <62160085@go.buu.ac.th>
2020-10-22 15:53:19 +0700

    Update Register.h

--- Register.h
+++ Register.h
@@ -1,25 +1,23 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 #include "include.h"
-#include"domainclass.h"
+#include "domainclass.h"
 class RegisterController{
     public:
         Member UserMember;
         MemberCard PassMember;
-       // ListMember *list_member;
         Member *head;
         Member *tail;
-        //Member *cur;
         RegisterController(); 
         ~RegisterController();
-        void SetMemberInformation(string f,string l,string t,string pw);
-        void SetMemberIndata(string firstname,string lastname,string tel,string password,string expire,string money);
-        void ShowUserInformation(string name,string pass);
-        void ShowExpireCard(string name,string pass);
-        void Showallmember();
-        void InputMoneyCard(double numMoney,string password);
+        void SetMemberInformation(string firstname,string lastname,string tel,string password);
+        void SetMemberInData(string firstname,string lastname,string tel,string password,string expire,string money);
+        void ShowUserInformation(string firstname,string password);
+        void ShowExpireCard(string firstname,string password);
+        void ShowAllMember();
+        void InputMoneyCard(double money,string password);
         void ShowRemainingAmount(Member *cur);
-        void savedata();
-        void Loaddata();
+        void SaveData();
+        void LoadData();
 };
 #endif
