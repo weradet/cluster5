cf4446495774d75cef8f4b9132cb14a1edca6e0f
weradet <70476442+weradet@users.noreply.github.com>
2020-10-24 00:36:11 +0700

    Update node_Addstation.cpp

--- node_Addstation.cpp
+++ node_Addstation.cpp
@@ -1,10 +1,10 @@
 #include"node_Addstation.h"
-		Node_Addstation::Node_Addstation(string Sid,string name,int c,string stat){
+		Node_Addstation::Node_Addstation(string name,string Sid ,int c,string stat){
 			StationID = Sid;
 			StationName = name;
             Status = stat;
             Cost = c;
 			link = NULL;
-			plink = NULL;
+			//plink = NULL;
 		}
 
