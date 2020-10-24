8fadf71be94c5509b3c0c2895dad01cb50c0700a
62160102 <62160102@go.buu.ac.th>
2020-10-24 14:39:34 +0700

    buy

--- Buy_ticket.h
+++ Buy_ticket.h
@@ -1,24 +1,20 @@
 #ifndef BUY_TICKET_H
 #define BUY_TICKET_H
 #include"include.h"
-//#include"station.h"
+#include"ViewCycleTimes.h"
 #include"Path.h"
 class Buy_ticket_controller{
-    private:
+    public:
         string Terminal;
         string Departure;
         int Cost;
         string Round;
-    public:
-       // Path line;
         Path *Head;
         Buy_ticket_controller();
         void Seach();
-        void Departure_Staion(string Origin);
-        void Terminal_Staion(string destination);
-        void Chose_Round();
         double Calculate();
         void Payment();
         void Print_Ticket();
+        void View(); 
 };
 #endif
